3c1e8224dee0f65c90731ccab81b77934655a964
BogdnaZyk <zykovbogdan07@gmail.com>
2023-10-24 13:27:28 +0300

    update package for new Apollo and SwiftPhoenixClient version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AbsintheSocketTransport",
-    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    platforms: [.iOS(.v14), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["AbsintheSocketTransport"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "1.3.0"),
-      .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "0.41.0")),
+      .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "5.3.1"),
+      .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "1.5.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
